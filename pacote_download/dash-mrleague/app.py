a576d1cf11241ea93a3af6b8df8a0a7d68f161b6
Tales Araújo Carvalho <114018304+talesaraujoc@users.noreply.github.com>
2023-03-09 15:26:37 -0300

    Update app.py
    add server -> dash web

--- pacote_download/dash-mrleague/app.py
+++ pacote_download/dash-mrleague/app.py
@@ -7,6 +7,7 @@ import dash_bootstrap_components as dbc
 
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
+server = app.server
 
 # ================================================================
 # ================================================================
